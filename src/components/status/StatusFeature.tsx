2f26c42dad80d28d0f4b7bddda09630db3f1f9cb
Rafael Noah Villavicencio <rafaelnoahvillavicencio@gmail.com>
2025-10-05 10:15:26 +0000

    the x tab is double there's behind (_for element <X>_)

--- src/components/status/StatusFeature.tsx
+++ src/components/status/StatusFeature.tsx
@@ -614,9 +614,6 @@ export default function StatusFeature() {
           <DialogContent className="p-0 m-0 border-0 w-screen h-[80vh] max-h-[600px] max-w-full sm:max-w-md flex flex-col gap-0 rounded-lg">
             <DialogHeader className="p-4 flex-row items-center justify-between border-b">
                 <DialogTitle>Create Status</DialogTitle>
-                <DialogClose asChild>
-                  <Button variant="ghost" size="icon"><X className="h-5 w-5" /></Button>
-                </DialogClose>
             </DialogHeader>
             <div className="flex-grow flex flex-col overflow-hidden px-6 pb-6">
               {uploaderContent()}
@@ -642,5 +639,3 @@ export default function StatusFeature() {
     </div>
   );
 }
-
-    
