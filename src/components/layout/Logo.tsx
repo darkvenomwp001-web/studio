0587943e523f24225a6c9e51d64c238c4d82d060
Rafael Noah Villavicencio <rafaelnoahvillavicencio@gmail.com>
2025-10-06 12:49:50 +0000

    remove this (_for element <BookOpenCheck>_)

--- src/components/layout/Logo.tsx
+++ src/components/layout/Logo.tsx
@@ -1,4 +1,3 @@
-
 'use client';
 
 import Link from 'next/link';
@@ -7,9 +6,6 @@ import { BookOpenCheck } from 'lucide-react';
 export default function Logo() {
   return (
     <Link href="/" className="flex items-center gap-2 group overflow-hidden">
-        <div className="relative h-8 w-8 flex items-center justify-center">
-            <BookOpenCheck className="h-8 w-8 text-primary transition-transform duration-500 ease-in-out transform group-hover:rotate-12" />
-        </div>
         <span className="text-2xl font-headline font-bold text-foreground">
             D4RKV3NOM
         </span>
