441215421522470707080d25dea154f933f12651
Rafael Noah Villavicencio <rafaelnoahvillavicencio@gmail.com>
2026-02-07 09:33:28 +0000

    remove this (_for element span_)

--- src/components/layout/Logo.tsx
+++ src/components/layout/Logo.tsx
@@ -7,9 +7,6 @@ export default function Logo() {
   return (
     <Link href="/" className="flex items-center gap-2 group">
       <AppLogo className="h-7 w-7" />
-      <span className="text-2xl font-headline font-bold text-foreground">
-        D4RKV3NOM
-      </span>
     </Link>
   );
 }
