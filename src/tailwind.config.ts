73df9aefeef30dc91c440742ca1382933c62dabb
Rafael Noah Villavicencio <rafaelnoahvillavicencio@gmail.com>
2026-05-15 08:56:18 +0000

    there is still a major error when typing a text, why is all the text doe

--- src/tailwind.config.ts
+++ src/tailwind.config.ts
@@ -136,5 +136,9 @@ export default {
       },
     },
   },
-  plugins: [require('tailwindcss-animate'), require('tailwind-scrollbar')],
+  plugins: [
+    require('tailwindcss-animate'), 
+    require('tailwind-scrollbar'),
+    require('@tailwindcss/typography')
+  ],
 } satisfies Config;
