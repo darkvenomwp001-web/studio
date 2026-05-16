1444f8fcda1c9cdfd246a915ccdb1099576bb521
Rafael Noah Villavicencio <rafaelnoahvillavicencio@gmail.com>
2026-05-16 07:04:12 +0000

    Try fixing this error: `Runtime Error: Error: Avatar is not defined. Err

--- src/app/write/edit-details/page.tsx
+++ src/app/write/edit-details/page.tsx
@@ -9,6 +9,7 @@ import { Input } from '@/components/ui/input';
 import { Textarea } from '@/components/ui/textarea';
 import { Label } from '@/components/ui/label';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { 
   Loader2, 
   Trash2, 
